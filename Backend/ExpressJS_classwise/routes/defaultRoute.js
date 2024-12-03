e70c88c7459a1abb4d180e86d9d82b695ff50105
SaiyedAadil <saiyedaadilcoresci787@gmail.com>
2024-12-03 13:00:27 +0530

    Add subrepo as submodule

--- Backend/ExpressJS_classwise/routes/defaultRoute.js
+++ Backend/ExpressJS_classwise/routes/defaultRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-router.use("/welcome", (req, res) => {
+router.use("/", (req, res) => {
     res.status(200).json({ message: "Welcome to My Backend Server!", success: true });
 });
 router.get("/health", (req, res) => {
